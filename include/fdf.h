4b45afb7eccbc428498de435a8f2a848651d0695
Fabiovvieira <fabiovv@hotmail.com>
2023-02-14 18:38:14 +0000

    test with mlx_key_hook

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: fvalli-v <fvalli-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 10:45:55 by fvalli-v          #+#    #+#             */
-/*   Updated: 2023/02/12 09:34:17 by fvalli-v         ###   ########.fr       */
+/*   Updated: 2023/02/14 17:39:09 by fvalli-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@
 # include "libft.h"
 # include "mlx.h"
 # include <math.h>
+
+# define ZOOMIN 105 //i
+# define ZOOMOUT 111 //o
+# define UP 119 //w
+# define DOWN 115 //s
+# define LEFT 97 //a
+# define RIGHT 100 //d
+# define RROT 114 //r
+# define LROT 116 //t
+# define ESC 65307 //ESC
+# define PROJECTION 65289 //TAB
 
 typedef struct	s_data {
 	void	*mlx;
--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: fvalli-v <fvalli-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 10:25:59 by fvalli-v          #+#    #+#             */
-/*   Updated: 2023/02/13 10:47:46 by fvalli-v         ###   ########.fr       */
+/*   Updated: 2023/02/14 18:37:14 by fvalli-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,85 +28,34 @@ void	draw_a_line(t_data *img, int x0, int y0, int x1, int y1)
 	// y0 *= img->zoom;
 	// x1 *= img->zoom;
 	// y1 *= img->zoom;
-
-	dx=x1-x0;
-	dy=y1-y0;
-
 	x=x0;
 	y=y0;
-
+	dx=x1-x0;
+	dy=y1-y0;
 	p=2*dy-dx;
-
-	while(x<x1)
+	if (x0 != x1)
 	{
-		if(p>=0)
+		while(x < x1)
 		{
 			my_mlx_pixel_put(img, x, y, 0x0000FFFF);
-			y=y+1;
-			p=p+2*dy-2*dx;
+			x++;
+			if(p>=0)
+			{
+				y++;
+				p=p+2*dy-2*dx;
+			}
+			else
+				p=p+2*dy;
 		}
-		else
-		{
-			my_mlx_pixel_put(img, x, y, 0x0000FFFF);
-			p=p+2*dy;
-		}
-		x=x+1;
 	}
-	// while (x1 <= x2)
-	// {
-	// 	my_mlx_pixel_put(img, x1, (m * x1 + c), 0x0000FFFF);
-
-	// 	x1++;
-	// }
-}
-
-void bresenham_line(t_data *img, int x1, int y1, int x2, int y2)
-{
-	// x1 *= img->zoom;
-	// y1 *= img->zoom;
-	// x2 *= img->zoom;
-	// y2 *= img->zoom;
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-    int x, y, p;
-
-    if (dx >= dy) {
-        if (x1 > x2) {
-            int t = x1; x1 = x2; x2 = t;
-            t = y1; y1 = y2; y2 = t;
-        }
-        x = x1; y = y1;
-        my_mlx_pixel_put(img, x, y, 0x0000FFFF);
-        p = 2 * dy - dx;
-        while (x < x2) {
-            x = x + 1;
-            if (p < 0) {
-                p = p + 2 * dy;
-            } else {
-                y = y + 1;
-                p = p + 2 * (dy - dx);
-            }
-            my_mlx_pixel_put(img, x, y, 0x0000FFFF);
-        }
-    } else {
-        if (y1 > y2) {
-            int t = x1; x1 = x2; x2 = t;
-            t = y1; y1 = y2; y2 = t;
-        }
-        x = x1; y = y1;
-        my_mlx_pixel_put(img, x, y, 0x0000FFFF);
-        p = 2 * dx - dy;
-        while (y < y2) {
-            y = y + 1;
-            if (p < 0) {
-                p = p + 2 * dx;
-            } else {
-                x = x + 1;
-                p = p + 2 * (dx - dy);
-            }
-            my_mlx_pixel_put(img, x, y, 0x0000FFFF);
-        }
-    }
+	else
+	{
+		while(y < y1)
+		{
+			my_mlx_pixel_put(img, x, y, 0x0000FFFF);
+			y++;
+		}
+	}
 }
 
 t_data	*init_data(void)
@@ -125,7 +74,7 @@ t_data	*init_data(void)
 	data->width_map = 0;
 	data->height_map = 0;
 	data->map = NULL;
-	data->zoom = 20;
+	data->zoom = 10;
 	return (data);
 }
 
@@ -202,13 +151,11 @@ void	parse_map(t_data *img, char *mapfile)
 		perror("Error on opening mapfile.");
 		exit(1);
 	}
-	// ft_printf("height = %d | width = %d \n", img->height_map, img->width_map);
 	while (i < img->height_map)
 	{
 		str = get_next_line(fd);
 		strs = ft_split(str, ' ');
 		map[i] = (int *)malloc(sizeof(int) * img->width_map);
-		// ft_printf("i = %d | j = %d | height = %d | width = %d \n",i, j, img->height_map, img->width_map);
 		while (j < img->width_map)
 		{
 			map[i][j] = ft_atoi(strs[j]);
@@ -247,7 +194,6 @@ void	print_map(t_data *img)
 
 	i = 0;
 	j = 0;
-	// ft_printf("height = %d | width = %d \n", img->height_map, img->width_map);
 	while (i < img->height_map)
 	{
 		while (j < img->width_map)
@@ -272,12 +218,13 @@ void	draw_file(t_data *img)
 		x = 0;
 		while (x < img->width_map)
 		{
-			bresenham_line(img, x, y, x + 1, y);
-			bresenham_line(img, x, y, x, y + 1);
+			draw_a_line(img, x, y, x + 1, y);
+			draw_a_line(img, x, y, x, y + 1);
 			x++;
 		}
 		y++;
 	}
+	mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
 }
 
 void	read_map(t_data *img, char *mapfile)
@@ -296,8 +243,6 @@ void	read_map(t_data *img, char *mapfile)
 
 int	ft_close(t_data *vars)
 {
-	// free_map(vars);
-	// mlx_destroy_image(vars->mlx, vars->img);
 	if (vars)
 	{
 		mlx_destroy_window(vars->mlx, vars->mlx_win);
@@ -307,28 +252,67 @@ int	ft_close(t_data *vars)
 		free_map(vars);
 		free(vars);
 	}
-
-	// free(vars);
 	exit (EXIT_SUCCESS);
+}
+
+void	print_line(int x, int y, t_data *img)
+{
+	// mlx_clear_window(img->mlx, img->mlx_win);
+	// (void)x;
+	// (void)y;
+	draw_a_line(img, x, y, 2 * x, 2 * y);
+	// mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
 }
 
 int	deal_key(int key, void *data)
 {
-	(void)data;
-	ft_printf("%d\n", key);
+	t_data	*img;
+	static int x = 100, y = 100;
+
+	img = (t_data *)data;
+	if (key == ESC)
+		ft_close(img);
+	else if (key == UP)
+	{
+		print_line(x, y, img);
+		// draw_a_line(img, 100, 100, 200, 200);
+		x += 10;
+	}
+	else
+		ft_printf("key pressed = %d\n", key);
+	mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
 	return(0);
+}
+
+
+
+int read_mouse(int button, int mouse_x, int mouse_y, void *param)
+{
+	int 		x;
+	int 		y;
+	t_data 		*img;
+
+	img = (t_data *) param;
+	if (!img)
+		return (-1);
+	if (mouse_x < 0 || mouse_y < 0 || mouse_x > img->width_map || mouse_y > img->height_map)
+		return (2);
+	x = (mouse_x % img->width_map);
+	y = (mouse_y % img->height_map);
+	ft_printf("mouse_x = %d | mouse_y = %d", x, y);
+	// mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
+	(void)button;
+	return (0);
 }
 
 int	main(int argc , char **argv)
 {
 	t_data	*img;
+	// int	i = 0;
+	// int	x = 100, y=100;
+
 
 	img = init_data();
-		/*
-	** After creating an image, we can call `mlx_get_data_addr`, we pass
-	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-	** then be set accordingly for the *current* data address.
-	*/
 	(void)argv;
 	if (argc != 2)
 	{
@@ -337,49 +321,17 @@ int	main(int argc , char **argv)
 	}
 	read_map(img, argv[1]);
 	print_map(img);
-
-
-
 	img->addr = mlx_get_data_addr(img->img, &(img->bits_per_pixel), &(img->line_length),
 								&(img->endian));
-
-
-
-	// draw_a_line(img, 50, 50 , 150, 150);
-
-	// bresenham_line(img, 200, 200, 300, 300);
-	draw_file(img);
-
-	// bresenham_line(img, 300, 10, 200, 300);
-
-	// bresenham_line(img, 200, 200, 300, 10);
-
-	// bresenham_line(img, 100, 100, 300, 10);
-
-
-	// while (i < 50)
-	// {
-	// 	while (j < 50)
-	// 	{
-	// 		color = color + 1000;
-	// 		my_mlx_pixel_put(&img, j, i, color);
-
-	// 		j++;
-	// 	}
-	// 	i++;
-	// 	j = 10;
-	// 	color = 0x0000FFFF;
-	// }
-
 	mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
-	// ft_printf("bpp = %d | line_length = %d | endian = %d \n", img.bits_per_pixel, img.line_length, img.endian);
-	// mlx_hook(img->mlx_win, 2, 1L<<0, ft_close, img);
+	draw_file(img);
+	// draw_a_line(img, 100, 100, 200, 100);
+	// draw_a_line(img, 100, 100, 100, 200); //error printing vertical line. Correct this.
+
+	// mlx_mouse_hook (img->mlx_win, read_mouse, img);
 	mlx_hook(img->mlx_win, 17, 0, ft_close, img);
-	// mlx_key_hook(img->mlx_win, deal_key, NULL);
+	mlx_key_hook(img->mlx_win, deal_key, img);
 	mlx_loop(img->mlx);
-	// free_map(img);
-	// mlx_destroy_image(img->mlx, img->img);
-	// mlx_destroy_window(img->mlx, img->mlx_win);
-	// free(img);
-	//Remember to free t_data
+	free(img);
+	return (0);
 }
